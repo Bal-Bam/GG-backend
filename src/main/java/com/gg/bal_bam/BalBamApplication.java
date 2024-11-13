12423452cf9f3729c84d32651648531d293fce72
eenzzi <purelej99@gmail.com>
2024-11-13 15:54:38 +0900

    project 생

---
+++ src/main/java/com/gg/bal_bam/BalBamApplication.java
@@ -0,0 +1,13 @@
+package com.gg.bal_bam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalBamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalBamApplication.class, args);
+	}
+
+}
