99d09f5c68507ff52769089debb9ee77196d546a
eenzzi <purelej99@gmail.com>
2024-11-14 10:53:16 +0900

    post entity 생성

---
+++ src/main/java/com/gg/bal_bam/domain/post/model/Post.java
@@ -0,0 +1,20 @@
+package com.gg.bal_bam.domain.post.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
