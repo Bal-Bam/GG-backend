dd909741df5cc9e0b9057c38f2a41bd66471a5de
eenzzi <purelej99@gmail.com>
2024-11-14 13:52:39 +0900

    엔티티 추가

--- src/main/java/com/gg/bal_bam/domain/user/model/User.java
+++ src/main/java/com/gg/bal_bam/domain/user/model/User.java
@@ -1,4 +1,12 @@
 package com.gg.bal_bam.domain.user.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    private Long id;
+
 }
