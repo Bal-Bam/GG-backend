0815260ebf4eab33cad0721b2661360393bc8e4e
eenzzi <purelej99@gmail.com>
2024-11-15 15:41:30 +0900

    스웨거의 spring security 인증을 건너뛰기 위한 SecurityConfig 추가

---
+++ src/main/java/com/gg/bal_bam/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.gg.bal_bam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 관련 경로 허용
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                )
+                .csrf(csrf -> csrf.disable()); // 필요에 따라 CSRF 비활성화
+
+        return http.build();
+    }
+
+}
