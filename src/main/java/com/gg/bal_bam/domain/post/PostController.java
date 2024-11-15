ba38767429e2a8a72ab2815d47a8788326b17f88
eenzzi <purelej99@gmail.com>
2024-11-15 16:08:48 +0900

    PostController 생성

--- src/main/java/com/gg/bal_bam/domain/post/PostController.java
+++ src/main/java/com/gg/bal_bam/domain/post/PostController.java
@@ -1,4 +1,20 @@
 package com.gg.bal_bam.domain.post;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Post", description = "게시글 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
 public class PostController {
+
+    @PostMapping
+    public void createPost() {
+    }
+
 }
