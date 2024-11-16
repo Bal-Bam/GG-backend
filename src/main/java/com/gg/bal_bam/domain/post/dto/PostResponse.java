2bb35c95561faa9ed7ca0d2bb1a32762aca696f8
eenzzi <purelej99@gmail.com>
2024-11-16 23:20:13 +0900

    postresponse dto 생성

---
+++ src/main/java/com/gg/bal_bam/domain/post/dto/PostResponse.java
@@ -0,0 +1,11 @@
+package com.gg.bal_bam.domain.post.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class PostResponse {
+
+    private Long postId;
+}
