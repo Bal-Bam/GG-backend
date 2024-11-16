fbd33e89944e3bf1cb2770fa7f51f897bc00f5a6
eenzzi <purelej99@gmail.com>
2024-11-16 22:21:57 +0900

    post 수정 서비스 개발

--- src/main/java/com/gg/bal_bam/domain/post/PostService.java
+++ src/main/java/com/gg/bal_bam/domain/post/PostService.java
@@ -1,6 +1,7 @@
 package com.gg.bal_bam.domain.post;
 
 import com.gg.bal_bam.domain.post.dto.PostRequest;
+import com.gg.bal_bam.domain.post.dto.PostUpdateRequest;
 import com.gg.bal_bam.domain.post.model.Post;
 import com.gg.bal_bam.domain.user.model.User;
 import com.gg.bal_bam.exception.CustomException;
@@ -52,5 +53,25 @@ public class PostService {
         );
 
         postRepository.save(post);
+
+        //태그 사용자 처리 코드 추가
+    }
+
+    // 게시글 수정
+    @Transactional
+    public void updatePost(Long postId, PostUpdateRequest postUpdateRequest, UUID userId) {
+
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new CustomException("게시글이 존재하지 않습니다. 게시글 ID: " + postId)
+        );
+
+        // 작성자인지 확인
+        if (!post.getUser().getId().equals(userId)) {
+            throw new CustomException("게시글 작성자만 수정할 수 있습니다.");
+        }
+
+        post.updatePost(postUpdateRequest.getContent());
+
+        //태그 사용자 처리 코드 추가
     }
 }
