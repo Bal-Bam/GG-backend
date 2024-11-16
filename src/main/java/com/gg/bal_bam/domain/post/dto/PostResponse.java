f937e762336c90df007d07220fb74743f2e3a384
eenzzi <purelej99@gmail.com>
2024-11-16 23:34:20 +0900

    UserResponse dto 수정

--- src/main/java/com/gg/bal_bam/domain/post/dto/PostResponse.java
+++ src/main/java/com/gg/bal_bam/domain/post/dto/PostResponse.java
@@ -1,11 +1,38 @@
 package com.gg.bal_bam.domain.post.dto;
 
+import com.gg.bal_bam.domain.user.dto.TaggedUserRequest;
+import com.gg.bal_bam.domain.user.dto.UserResponse;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class PostResponse {
 
     private Long postId;
+    private UserResponse user; // 게시글 작성자
+    private String content;
+    private Double latitude;
+    private Double longitude;
+    private List<> taggedUsers; // 태그된 사용자 목록
+    private LocalDateTime createdAt;
+    private List<PostResponse> childPosts; // 자식 게시글 목록
+
+    private PostResponse(Long postId, UserResponse user, String content, Double latitude, Double longitude, List<TaggedUserRequest> taggedUsers, LocalDateTime createdAt, List<PostResponse> childPosts) {
+        this.postId = postId;
+        this.user = user;
+        this.content = content;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.taggedUsers = taggedUsers;
+        this.createdAt = createdAt;
+        this.childPosts = childPosts;
+    }
+
+    public static PostResponse of(Long postId, UserResponse user, String content, Double latitude, Double longitude, List<TaggedUserRequest> taggedUsers, LocalDateTime createdAt, List<PostResponse> childPosts) {
+        return new PostResponse(postId, user, content, latitude, longitude, taggedUsers, createdAt, childPosts);
+    }
 }
