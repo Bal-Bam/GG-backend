5b00002ca23647ff0021e72264cd6f0bff9e4fc3
eenzzi <purelej99@gmail.com>
2024-11-18 10:47:52 +0900

    LikeRepository 생성

---
+++ src/main/java/com/gg/bal_bam/domain/like/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.gg.bal_bam.domain.like;
+
+import com.gg.bal_bam.domain.like.model.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+
+}
