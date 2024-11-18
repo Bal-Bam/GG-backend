2dccd17290385d8ad99a9acfdb66e08c6fae2661
eenzzi <purelej99@gmail.com>
2024-11-18 09:56:19 +0900

    parentPost로 childPosts 목록 가져오기

--- src/main/java/com/gg/bal_bam/domain/post/repository/PostRepository.java
+++ src/main/java/com/gg/bal_bam/domain/post/repository/PostRepository.java
@@ -3,6 +3,10 @@ package com.gg.bal_bam.domain.post.repository;
 import com.gg.bal_bam.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByParentPost(Post parentPost);
 
 }
