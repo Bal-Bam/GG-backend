3236dafe4de3717e0c883a8138c5d5f81f94e212
eenzzi <purelej99@gmail.com>
2024-11-18 09:56:46 +0900

    post로 PostTag 목록 받아오기

--- src/main/java/com/gg/bal_bam/domain/post/repository/PostTagRepository.java
+++ src/main/java/com/gg/bal_bam/domain/post/repository/PostTagRepository.java
@@ -4,7 +4,12 @@ import com.gg.bal_bam.domain.post.model.Post;
 import com.gg.bal_bam.domain.post.model.PostTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostTagRepository extends JpaRepository<PostTag, Long> {
 
     void deleteByPost(Post post);
+
+    List<PostTag> findByPost(Post post);
+
 }
