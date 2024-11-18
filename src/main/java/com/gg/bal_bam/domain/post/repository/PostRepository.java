84c92be8a51774c850fb36ee405b00c58a0764a1
eenzzi <purelej99@gmail.com>
2024-11-18 16:23:48 +0900

    목록 조회 쿼리 메서드 추가

--- src/main/java/com/gg/bal_bam/domain/post/repository/PostRepository.java
+++ src/main/java/com/gg/bal_bam/domain/post/repository/PostRepository.java
@@ -2,11 +2,28 @@ package com.gg.bal_bam.domain.post.repository;
 
 import com.gg.bal_bam.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
 import java.util.List;
+import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByParentPost(Post parentPost);
+
+    @Query(value = "SELECT * FROM posts " +
+            "WHERE ST_Distance_Sphere(point(longitude, latitude), point(:longitude, :latitude)) <= :distance " +
+            "ORDER BY created_at DESC " +
+            "LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Post> findNearbyPosts(@Param("latitude") double latitude,
+                               @Param("longitude") double longitude,
+                               @Param("distance") double distance,
+                               @Param("limit") int limit,
+                               @Param("offset") int offset);
+
+    @Query("SELECT p FROM Post p WHERE p.user.id IN :userIds ORDER BY p.createdAt DESC")
+    List<Post> findPostsByUserIds(@Param("userIds") List<UUID> userIds, Pageable pageable);
 
 }
