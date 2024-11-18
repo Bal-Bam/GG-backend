9703042255b2e478f1f296c3d94da9797409c6a5
eenzzi <purelej99@gmail.com>
2024-11-18 14:59:58 +0900

    LikeService에 userrepository 코드 추가

--- src/main/java/com/gg/bal_bam/domain/like/LikeService.java
+++ src/main/java/com/gg/bal_bam/domain/like/LikeService.java
@@ -4,8 +4,11 @@ import com.gg.bal_bam.domain.like.dto.LikeResponse;
 import com.gg.bal_bam.domain.like.model.Like;
 import com.gg.bal_bam.domain.post.model.Post;
 import com.gg.bal_bam.domain.post.repository.PostRepository;
+import com.gg.bal_bam.domain.user.UserRepository;
 import com.gg.bal_bam.domain.user.model.User;
+import com.gg.bal_bam.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,16 +21,20 @@ public class LikeService {
 
     private final PostRepository postRepository;
     private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public LikeResponse like(Long postId, UUID userId) {
 
         Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. PostId: " + postId));
+                .orElseThrow(() -> new CustomException("해당 게시글이 존재하지 않습니다. PostId: " + postId));
 
-        User user = null; // userRepository 개발 되면 추가
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException("해당 유저가 존재하지 않습니다. UserId: " + userId));
+
 
         Like like = likeRepository.findByUserAndPost(user, post).orElse(null);
+
         if (like == null) {
             like = Like.createLike(user, post);
             likeRepository.save(like);
