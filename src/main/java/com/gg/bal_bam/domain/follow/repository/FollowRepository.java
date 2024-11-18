7ffa5a01e25100fc5d8c7cc26bccf009fbf67803
eenzzi <purelej99@gmail.com>
2024-11-18 17:50:01 +0900

    repository 메서드 추가

--- src/main/java/com/gg/bal_bam/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/gg/bal_bam/domain/follow/repository/FollowRepository.java
@@ -1,7 +1,14 @@
 package com.gg.bal_bam.domain.follow.repository;
 
 import com.gg.bal_bam.domain.follow.model.Follow;
+import com.gg.bal_bam.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    boolean existsByFollowerAndFollowing(User follower, User following);
+
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 }
