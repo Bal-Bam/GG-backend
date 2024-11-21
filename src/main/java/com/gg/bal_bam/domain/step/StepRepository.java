8b17e983f51820bae3d47cc614d96058c7baa5ae
eenzzi <purelej99@gmail.com>
2024-11-21 11:45:58 +0900

    Step repository 생성

---
+++ src/main/java/com/gg/bal_bam/domain/step/StepRepository.java
@@ -0,0 +1,7 @@
+package com.gg.bal_bam.domain.step;
+
+import com.gg.bal_bam.domain.step.model.Step;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StepRepository extends JpaRepository<Step, Long> {
+}
